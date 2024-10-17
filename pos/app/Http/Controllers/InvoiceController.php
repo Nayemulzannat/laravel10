f75c140adce439f7991d361c612ede2aaca8f381
MD Nayem <nayemtp@gmail.com>
2024-10-17 23:26:16 +0600

    invoice start

--- pos/app/Http/Controllers/InvoiceController.php
+++ pos/app/Http/Controllers/InvoiceController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\invoice;
+use App\Models\invoiceProduct;
+use Exception;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class InvoiceController extends Controller
 {
@@ -16,9 +20,55 @@ class InvoiceController extends Controller
     {
         return view('pages.dashboard.sale-page');
     }
+    function invoiceCreate(Request $request)
+    {
+
+        DB::beginTransaction();
+
+        try {
+            $user_id = $request->header('userID');
+            $total = $request->input('total');
+            $discount = $request->input('discount');
+            $vat = $request->input('vat');
+            $payable = $request->input('payable');
+            $customer_id = $request->input('customer_id');
+
+            $invoice = invoice::create([
+                'total' => $total,
+                'discount' => $discount,
+                'vat' => $vat,
+                'payable' => $payable,
+                'user_id' => $user_id,
+                'customer_id' => $customer_id,
+            ]);
+
+            $invoiceID = $invoice->id;
 
 
+            $products = $request->input('products');
 
+            foreach ($products as $EachProduct) {
+                invoiceProduct::create([
+                    'invoice_id' => $invoiceID,
+                    'user_id' => $user_id,
+                    'product_id' => $EachProduct['product_id'],
+                    'qty' =>  $EachProduct['qty'],
+                    'sale_price' =>  $EachProduct['sale_price'],
+                ]);
+            }
 
-    
+            DB::commit();
+
+            return 1;
+        } catch (Exception $e) {
+            DB::rollBack();
+            return 0;
+        }
+    }
+
+    function invoiceSelect(Request $request)
+    {
+        $user_id = $request->header('userID');
+        return invoice::where('user_id', $user_id)->with('customer')->get();
+    }
 }
--- pos/app/Models/invoice.php
+++ pos/app/Models/invoice.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class invoice extends Model
 {
-    use HasFactory;
+    protected $fillable = ['total', 'discount', 'vat', 'payable', 'user_id', 'customer_id'];
+
+    function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
