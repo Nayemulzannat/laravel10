fb63e24b6a9cb85804f167f4ed5794f9c4628eec
nayem <nayemtp@gmail.com>
2025-03-22 23:48:15 +0600

    helper set

--- apple-shop/app/Models/Policy.php
+++ apple-shop/app/Models/Policy.php
@@ -1,21 +1,8 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
-/**
- * @method static where(string $string, mixed $id)
- */
-class Product extends Model
-{
-    public function brand(): BelongsTo
-    {
-        return $this->belongsTo(Brand::class);
-    }
-    public function category(): BelongsTo
-    {
-        return $this->belongsTo(Category::class);
-    }
-}
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model {}
--- apple-shop/app/Models/Product.php
+++ apple-shop/app/Models/Product.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+/**
+ * @method static where(string $string, mixed $id)
+ */
+
 
 class Product extends Model
 {
-    use HasFactory;
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
